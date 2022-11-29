82b9e33ef0890c61879973e28e3004c26fd1410a
jianhu <jianhu@risinghf.com>
2022-11-29 14:24:33 +0800

    修改: 模板

--- internal/models/template.go
+++ internal/models/template.go
@@ -2,4 +2,4 @@ package models
 
 var sqlExample = `SELECT setval('action_record_logs_id_seq', (SELECT MAX(id) FROM action_record_logs)+1);`
 
-const IdSeqSetValSqlTpl = `setval({{ .sequenceName }},(SELECT MAX(id) FROM{{ .tableName }})+1);`
+const IdSeqSetValSqlTpl = `SELECT setval('{{ .sequenceName }}',(SELECT MAX(id) FROM {{ .tableName }})+1);`
